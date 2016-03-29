b13e4d76e7a020c67608763b6f0e8e29ed3ff6eb
Efrix <info@efrix.net>
2016-03-29 16:34:49 -0600

    agregar rejillas

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,20 @@ jQuery(document).ready(function($){
   $('#boton_menu').click(function(){
     $(this).next().slideToggle();
   });
+
   $(window).load(function() {
     $('#slider').nivoSlider({
       controlNav: false,
     });
   });
+
+//Accordion
+
+    $(".accordion .descripcion").slideUp();
+    $(".accordion .titulo").click(function(){
+    $(this).next(".accordion .descripcion").slideToggle("fast");
+  });
+
   //GOOGLE-MAPS
   $('.maps').click(function () {
     $('.maps iframe').css("pointer-events", "auto");
